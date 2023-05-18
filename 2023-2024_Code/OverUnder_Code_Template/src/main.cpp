abdf5712682b6d3bf2b83c306e862845eee997a8
EliteKnightOzzie <127443624+EliteKnightOzzie@users.noreply.github.com>
2023-05-18 08:51:15 -0400

    Add files via upload

---
+++ 2023-2024_Code/OverUnder_Code_Template/src/main.cpp
@@ -0,0 +1,21 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       Names                                                     */
+/*    Created:      dd/mm/yyyy                                                */
+/*    Description:  Write description here                                    */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+using namespace vex;
+
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+  
+}
